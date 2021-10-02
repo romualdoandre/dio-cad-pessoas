add18d60954115fa2baf5bb76c8abb8a0928300c
Romualdo <romualdoandre@gmail.com>
2021-10-02 16:03:57 -0300

    entidades repository

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,12 +1,23 @@
 package one.digitalinnovation.personapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.personapi.repository.PersonRepository;
+
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+	
+	private PersonRepository personRepository;
+	@Autowired
+	public PersonController(PersonRepository personRepository) {
+		super();
+		this.personRepository = personRepository;
+	}
+
 	@GetMapping
 	String getBook() {
 		return "Hello";
